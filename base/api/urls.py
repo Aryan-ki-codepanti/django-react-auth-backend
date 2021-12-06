10800ac7279084b05ff8160cd8e69ccfd1936aa4
Aryan Sethi <aryan.rdps@gmail.com>
2021-12-06 22:07:25 +0530

    Customized token view to send username also

--- base/api/urls.py
+++ base/api/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 from base.api import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', views.getRoutes, name="GetRoutes"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
